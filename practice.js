00f41601a762a4e70a14d1d5eb365f959ece1961
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-09-18 17:35:28 -0600

    year of birth function

---
+++ practice.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
+  return 2019 - age;
+}
+
+
+function createGreeting(name, age) {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+  if (typeof (name) !== 'string' || typeof (age) !== 'number') {
+    throw new TypeError('Invalid Entry');
+  }
+  let yearOfBirth = getYearOfBirth(age);
+  return `Hi my name is ${name}, I am ${age} years old. I will be ${age + 1} in 2020. I was born in ${yearOfBirth}`;
+}
+
+try {
+  const greeting1 = createGreeting('Shan', 27);
+  console.log(greeting1);
+} catch (err) {
+  console.error(err.message);
+}
+
+
